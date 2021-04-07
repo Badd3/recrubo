c4e439764b665fcb7344e9f3f3b3122b962c0cde
Amin Pasalic <pasalicamin@gmail.com>
2021-04-07 21:21:34 +0200

    Hero en Onze Diensten

--- page-home.php
+++ page-home.php
@@ -1,16 +1,55 @@
 <?php get_header(); ?>
-<section class="landing align-center d-flex">
-    <div class="landing-left flex-50">
+<section class="hero align-center d-flex">
+    <div class="hero-left">
         <h1>Wij ontzorgen uw HR personeel</h1>
         <p>Wij van Recrubo helpen u graag bij het ontzorgen van uw HR personeel, zodat ze meer tijd overhouden!</p>
-        <div class="landing-cta">
-            <a class="button shadow blue" href="/contact">Demo aanvragen</a>
-            <a class="button" href="">Meer over HR chatbots</a>
+        <div class="hero-cta">
+            <a class="button bg-white shadow blue" href="/contact">Vraag een demo aan</a>
+            <a class="button black" href="">Kom met ons in contact</a>
         </div>
     </div>
-    <div class="landing-right flex-50">
+</section>
 
-
+<section class="onze-diensten d-flex row-wrap">
+    <h2 class="flex-100 "> Onze Diensten</h2>
+    <div class="column-wrap flex-100 d-flex row-wrap space-between">
+        <div class="column chatbot">
+            <a href="">
+                <div class="icon">
+                    <img src="<?php THEMEDIR; ?>/wp-content/themes/recrubo/dist/images/chat.svg" alt="" srcset="">
+                </div>
+                <h3>Chatbot</h3>
+                <span>We are happy to hear you had a positive experience! </span>
+            </a>
+        </div>
+        <div class="column automatisering">
+            <a href="">
+                <div class="icon">
+                    <img src="<?php THEMEDIR; ?>/wp-content/themes/recrubo/dist/images/repeat.svg" alt="" srcset="">
+                </div>
+                <h3>Automatisering</h3>
+                <span>We are happy to hear you had a positive experience! </span>
+            </a>
+        </div>
+        <div class="column platformen">
+            <a href="">
+                <div class="icon">
+                    <img src="<?php THEMEDIR; ?>/wp-content/themes/recrubo/dist/images/layers.svg" alt="" srcset="">
+                </div>
+                <h3>Platformen</h3>
+                <span>We are happy to hear you had a positive experience!</span>
+            </a>
+        </div>
+        <div class="column marketing">
+            <a href="">
+                <div class="icon">
+                    <img src="<?php THEMEDIR; ?>/wp-content/themes/recrubo/dist/images/fast.svg" alt="" srcset="">
+                </div>
+                <h3>Marketing</h3>
+                <span>We are happy to hear you had a positive experience! </span>
+            </a>
+        </div>
     </div>
+
 </section>
 <?php get_footer(); ?>
