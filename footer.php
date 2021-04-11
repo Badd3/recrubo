b11d15fcdc1e95337561499a87452a74c19c734f
Amin Pasalic <pasalicamin@gmail.com>
2021-04-11 11:02:00 +0200

    header and footer

--- footer.php
+++ footer.php
@@ -3,14 +3,9 @@
 
     <footer class="site-footer">
         <div class="container">
-            <hr>
             <aside class="footer-widgets widget-area">
                 <?php dynamic_sidebar('footer-widgets'); ?>
             </aside>
-            <hr>
-            <div class="site-info">
-                <a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>, <?php bloginfo('description') ?>
-            </div>
         </div>
     </footer>
 
